400818795cf35e003d3e40f003511c3f3b3c0f60
Tej-try <153263303+Tej-try@users.noreply.github.com>
2024-03-28 17:25:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ fetch(url, {
             
             'fname':  fname,
              'sname':sname,
-            'percentage':per,
+            '"percentage "':per,
              
         }
     })
